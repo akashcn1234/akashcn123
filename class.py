584474dac7ca51180d1782f13782479fe24e11b6
MCA <you@example.com>
2022-12-21 09:43:59 +0530

    Initial commit

---
+++ class.py
@@ -0,0 +1,21 @@
+class A:
+    def features1(self):
+        print("AKASH CEE YEN")
+    def features2(self):
+        print("akash")
+class B(A):
+    def features3(self):
+        print("CEE_YEN")
+    def features4(self):
+        print("****")
+b=B()
+b.features1()
+b.features2()
+b.features3()
+b.features4()
+
+
+
+
+
+
